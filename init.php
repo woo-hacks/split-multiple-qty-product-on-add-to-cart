7b1908143cbe05c4432f2d83eb982094d0f62482
Karthik Thayyil <me@kt12.in>
2018-09-13 17:45:38 +0530

    :pencil: typo

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:   Woo-Tip #2 - Split Product with more than 1 quanitity while doing add to cart
- * Description:  Try to add more than one quantiy to cart and the it will be split into multiple cart item
+ * Description:  Try to add more than one quantiy to cart and it will be split into multiple cart item.
  * Version:     0.0.2
  * Author:      KT-12
  * Author URI:  https://kt12.in/
